c74d2495d81795cf4efaf5f4b34c50817f55ce29
Spoekle <thiangraber@gmail.com>
2024-06-13 20:42:51 +0200

    background suff

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Navbar from './Navbar';
 
 const HomePage = () => {
     const [user, setUser] = useState(null);
@@ -25,32 +26,37 @@ const HomePage = () => {
     }, []);
 
   return (
-    <div className="bg-gradient-to-r from-cc-red to-cc-blue min-h-screen flex flex-col justify-center items-center text-white">
-      <h1 className="text-5xl font-bold mb-8">Welcome to ClipSesh!</h1>
-      <p className="text-xl mb-8">A new generation for Seasonal Highlights!</p>
-      <p className="text-xl mb-8">Content Cubes come together to rate YOUR clips for the highlights!</p>
-      <p className="text-xl mb-8">Now everyone can upvote or downvote clips to influence how the highlights will play out</p>
-      <div className="flex justify-between w-2/5 mt-8">
-        {user ? (
-          <button className="bg-cc-red text-white font-bold py-3 px-6 mr-4 rounded-lg shadow-lg transition duration-300">
-          Hello, {user.username}!
-          </button>
-        ) : (
+    <>
+      <div className="bg-gradient-to-r from-cc-red to-cc-blue min-h-screen flex flex-col justify-center items-center text-white">
+        <div className='absolute top-0 w-full'>
+          <Navbar />
+        </div>
+        <h1 className="text-5xl font-bold mb-8 text-center">Welcome to ClipSesh!</h1>
+        <p className="text-xl mb-8 text-center">A new generation for Seasonal Highlights!</p>
+        <p className="text-xl mb-8 text-center">Content Cubes come together to rate YOUR clips for the highlights!</p>
+        <p className="text-xl mb-8 text-center">Now everyone can upvote or downvote clips to influence how the highlights will play out</p>
+        <div className="flex justify-between w-2/5 mt-8">
+          {user ? (
+            <button className="bg-cc-red text-white font-bold py-3 px-6 mr-4 rounded-lg shadow-lg transition duration-300">
+              Hello, {user.username}!
+            </button>
+          ) : (
             <>
               <Link to="/login">
-                  <button className="bg-white text-cc-blue hover:bg-cc-red hover:text-white font-bold py-3 px-6 mr-4 rounded-lg shadow-lg transition duration-300">
+                <button className="bg-white text-cc-blue hover:bg-cc-red hover:text-white font-bold py-3 px-6 mr-4 rounded-lg shadow-lg transition duration-300">
                   Login!
-                  </button>
+                </button>
               </Link>
             </>
-            )}
-        <Link to="/view">
-          <button className="bg-white text-cc-blue hover:bg-cc-red hover:text-white font-bold py-3 px-6 mr-4 rounded-lg shadow-lg transition duration-300">
-            View Clips
-          </button>
-        </Link>
+          )}
+          <Link to="/view">
+            <button className="bg-white text-cc-blue hover:bg-cc-red hover:text-white font-bold py-3 px-6 mr-4 rounded-lg shadow-lg transition duration-300">
+              View Clips
+            </button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
