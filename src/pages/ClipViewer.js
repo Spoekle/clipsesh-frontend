b23f5667178aed31a17049e936b8c4a0f84845a9
Spoekle <thiangraber@gmail.com>
2024-07-04 03:48:27 +0200

    added new banner and better styling

--- src/pages/ClipViewer.js
+++ src/pages/ClipViewer.js
@@ -6,6 +6,7 @@ import LoadingBar from 'react-top-loading-bar';
 import Pagination from '@mui/material/Pagination';
 import background from '../media/background.jpg';
 import placeholder from '../media/placeholder.png';
+import banner1 from '../media/banner1.png';
 import Navbar from './components/Navbar';
 import RatingModal from './components/clipViewer/RatingModal';
 import DeniedClips from './components/clipViewer/DeniedClips';
@@ -215,9 +216,11 @@ function ClipViewer() {
         <Navbar />
       </div>
       <div className="flex h-96 justify-center items-center" style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover' }}>
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="text-4xl font-bold mb-4 text-center">Clip Viewer</h1>
-          <h1 className="text-3xl mb-4 text-center">Rate the clips!</h1>
+        <div className="flex bg-white/20 backdrop-blur-lg justify-center items-center w-full h-full">
+          <div className="flex flex-col justify-center items-center">
+            <h1 className="text-4xl font-bold mb-4 text-center">Clip Viewer</h1>
+            <h1 className="text-3xl mb-4 text-center">Rate the clips!</h1>
+          </div>
         </div>
       </div>
       <div className="grid justify-items-center text-white p-4 pt-8 bg-neutral-200 dark:bg-neutral-900 transition duration-200 justify-center items-center">
@@ -237,114 +240,116 @@ function ClipViewer() {
             </select>
           </div>
         </div>
-        <div className="container mt-4 bg-black/30 dark:bg-white/30 rounded-md">
-          <h2 className="p-4 text-center bg-neutral-800 dark:text-neutral-800 dark:bg-white text-white transition duration-200 backdrop-blur-sm rounded-t-md text-2xl font-bold mb-4">Clips</h2>
-          <div className="flex justify-center">
-            <div className="items-center justify-center bg-white text-neutral-900 rounded-md py-2 px-4">
-              <Pagination
-                showFirstButton showLastButton
-                count={totalPages}
-                page={currentPage}
-                onChange={(e, page) => paginate(page)}
-                color="standard"
-              />
+        <div className="container mt-4 justify-center items-center rounded-md" style={{ backgroundImage: `url(${banner1})`, backgroundSize: 'cover' }}>
+          <div className='h-full w-full bg-white/20 backdrop-blur-lg rounded-md'>
+            <h2 className="p-4 text-center bg-neutral-800 dark:text-neutral-800 dark:bg-white text-white transition duration-200 backdrop-blur-sm rounded-t-md text-2xl font-bold drop-shadow-md mb-4">Clips</h2>
+            <div className="flex justify-center">
+              <div className="items-center justify-center bg-white text-neutral-900 rounded-md py-2 px-4">
+                <Pagination
+                  showFirstButton showLastButton
+                  count={totalPages}
+                  page={currentPage}
+                  onChange={(e, page) => paginate(page)}
+                  color="standard"
+                />
+              </div>
             </div>
-          </div>
-          <div className="justify-center grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-            {isLoading ? (
-              Array.from({ length: 6 }).map((_, index) => (
-                <div key={index} className="p-4 relative animate-pulse">
-                  <div className="overflow-hidden w-full text-center relative">
-                    {isLoggedIn && (
-                      <div className="flex justify-center">
-                        <div className='absolute top-0 right-0 z-40 p-2 bg-white text-neutral-900 dark:bg-neutral-800 dark:text-white transition duration-200 rounded-md'>
+            <div className="justify-center grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+              {isLoading ? (
+                Array.from({ length: 6 }).map((_, index) => (
+                  <div key={index} className="p-4 relative animate-pulse drop-shadow-md">
+                    <div className="overflow-hidden w-full text-center relative">
+                      {isLoggedIn && (
+                        <div className="flex justify-center">
+                          <div className='absolute top-0 right-0 z-40 p-2 bg-white text-neutral-900 dark:bg-neutral-800 dark:text-white transition duration-200 rounded-md'>
+                            <button
+                              className="text-lg font-bold p-1 bg-neutral-200 text-neutral-900 dark:bg-neutral-700 dark:text-white transition duration-200 hover:text-blue-500 rounded-sm"
+                            >
+                              Rating!
+                            </button>
+                          </div>
+                        </div>
+                      )}
+                      <div className="absolute flex justify-center top-0 left-0 z-30 text-lg font-bold bg-white text-neutral-900 dark:bg-neutral-800 dark:text-white transition duration-200 p-2 rounded-md text-center">Cube Community</div>
+                      <div className='rounded-t-lg bg-white dark:bg-neutral-800 transition duration-200 p-2'>
+                        <img src={placeholder} alt="Logo" className="w-full rounded-t-lg border-8 border-white opacity-50" />
+                      </div>
+                    </div>
+                    <div className="w-full flex justify-center bg-white dark:bg-neutral-900 transition duration-200 rounded-b-lg px-4 pt-2 pb-4">
+                      <button
+                        className="w-1/2 text-green-500 dark:text-green-800 flex items-center justify-center bg-neutral-100 dark:bg-neutral-800 hover:text-white hover:bg-green-500 dark:hover:bg-green-800 transition duration-300 py-2 px-6 rounded-l-md"
+                      >
+                        <FaArrowUp className="mr-1" /> 420
+                      </button>
+                      <button
+                        className="w-1/2 text-red-500 dark:text-red-800 flex items-center justify-center bg-neutral-100 dark:bg-neutral-800 hover:text-white hover:bg-red-500 dark:hover:bg-red-800 transition duration-300 py-2 px-6 rounded-r-md"
+                      >
+                        <FaArrowDown className="mr-1" /> 69
+                      </button>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                currentClips.length > 0 ? (
+                  currentClips
+                    .filter(clip => {
+                      if (isLoggedIn) {
+                        const ratingData = ratings[clip._id];
+                        return ratingData && !ratingData.ratingCounts.some(rateData => rateData.users.some(ratingUser => ratingUser.userId === user._id)) && ratingData.ratingCounts.some(rateData => rateData.rating === 'deny' && rateData.count < denyThreshold);
+                      } else {
+                        return true;
+                      }
+                    })
+                    .map(clip => (
+                      <div key={clip._id} className="p-4 relative animate-fade drop-shadow-md">
+                        <div className="overflow-hidden w-full text-center relative">
+                          {isLoggedIn && (
+                            <div className="flex justify-center">
+                              <div className='absolute top-0 right-0 z-40 p-2 bg-white text-neutral-900 dark:bg-neutral-800 dark:text-white transition duration-200 rounded-md'>
+                                <button
+                                  className="text-lg font-bold p-1 bg-neutral-200 text-neutral-900 dark:bg-neutral-700 dark:text-white transition duration-200 hover:text-blue-500 rounded-sm"
+                                  onClick={() => setExpandedClip(clip._id)}
+                                >
+                                  Rating!
+                                </button>
+                              </div>
+                            </div>
+                          )}
+                          <div className="absolute flex justify-center top-0 left-0 z-30 text-lg font-bold bg-white text-neutral-900 dark:bg-neutral-800 dark:text-white transition duration-200 p-2 rounded-md text-center">{clip.streamer}</div>
+                          <div className='rounded-t-lg bg-white dark:bg-neutral-800 transition duration-200 p-2'>
+                            <video
+                              className="w-full rounded-lg border-white dark:border-neutral-800 transition duration-200"
+                              src={`https://api.spoekle.com${clip.url}`}
+                              controls
+                            >
+                            </video>
+                          </div>
+                        </div>
+                        <div className="w-full flex justify-center bg-white dark:bg-neutral-900 transition duration-200 rounded-b-lg px-4 pt-2 pb-4">
                           <button
-                            className="text-lg font-bold p-1 bg-neutral-200 text-neutral-900 dark:bg-neutral-700 dark:text-white transition duration-200 hover:text-blue-500 rounded-sm"
+                            className="w-1/2 text-green-500 dark:text-green-800 flex items-center justify-center bg-neutral-100 dark:bg-neutral-800 hover:text-white hover:bg-green-500 dark:hover:bg-green-800 transition duration-300 py-2 px-6 rounded-l-md"
+                            onClick={() => upvoteClip(clip._id)}
                           >
-                            Rating!
+                            <FaArrowUp className="mr-1" /> {clip.upvotes}
+                          </button>
+                          <button
+                            className="w-1/2 text-red-500 dark:text-red-800 flex items-center justify-center bg-neutral-100 dark:bg-neutral-800 hover:text-white hover:bg-red-500 dark:hover:bg-red-800 transition duration-300 py-2 px-6 rounded-r-md"
+                            onClick={() => downvoteClip(clip._id)}
+                          >
+                            <FaArrowDown className="mr-1" /> {clip.downvotes}
                           </button>
                         </div>
                       </div>
-                    )}
-                    <div className="absolute flex justify-center top-0 left-0 z-30 text-lg font-bold bg-white text-neutral-900 dark:bg-neutral-800 dark:text-white transition duration-200 p-2 rounded-md text-center">Cube Community</div>
-                    <div className='rounded-t-lg bg-white dark:bg-neutral-800 transition duration-200 p-2'>
-                      <img src={placeholder} alt="Logo" className="w-full rounded-t-lg border-8 border-white opacity-50" />
-                    </div>
-                  </div>
-                  <div className="w-full flex justify-center bg-white dark:bg-neutral-900 transition duration-200 rounded-b-lg px-4 pt-2 pb-4">
-                    <button
-                      className="w-1/2 text-green-500 dark:text-green-800 flex items-center justify-center bg-neutral-100 dark:bg-neutral-800 hover:text-white hover:bg-green-500 dark:hover:bg-green-800 transition duration-300 py-2 px-6 rounded-l-md"
-                    >
-                      <FaArrowUp className="mr-1" /> 420
-                    </button>
-                    <button
-                      className="w-1/2 text-red-500 dark:text-red-800 flex items-center justify-center bg-neutral-100 dark:bg-neutral-800 hover:text-white hover:bg-red-500 dark:hover:bg-red-800 transition duration-300 py-2 px-6 rounded-r-md"
-                    >
-                      <FaArrowDown className="mr-1" /> 69
-                    </button>
-                  </div>
-                </div>
-              ))
-            ) : (
-              currentClips.length > 0 ? (
-                currentClips
-                  .filter(clip => {
-                    if (isLoggedIn) {
-                      const ratingData = ratings[clip._id];
-                      return ratingData && !ratingData.ratingCounts.some(rateData => rateData.users.some(ratingUser => ratingUser.userId === user._id)) && ratingData.ratingCounts.some(rateData => rateData.rating === 'deny' && rateData.count < denyThreshold);
-                    } else {
-                      return true;
-                    }
-                  })
-                  .map(clip => (
-                    <div key={clip._id} className="p-4 relative animate-fade">
-                      <div className="overflow-hidden w-full text-center relative">
-                        {isLoggedIn && (
-                          <div className="flex justify-center">
-                            <div className='absolute top-0 right-0 z-40 p-2 bg-white text-neutral-900 dark:bg-neutral-800 dark:text-white transition duration-200 rounded-md'>
-                              <button
-                                className="text-lg font-bold p-1 bg-neutral-200 text-neutral-900 dark:bg-neutral-700 dark:text-white transition duration-200 hover:text-blue-500 rounded-sm"
-                                onClick={() => setExpandedClip(clip._id)}
-                              >
-                                Rating!
-                              </button>
-                            </div>
-                          </div>
-                        )}
-                        <div className="absolute flex justify-center top-0 left-0 z-30 text-lg font-bold bg-white text-neutral-900 dark:bg-neutral-800 dark:text-white transition duration-200 p-2 rounded-md text-center">{clip.streamer}</div>
-                        <div className='rounded-t-lg bg-white dark:bg-neutral-800 transition duration-200 p-2'>
-                          <video
-                            className="w-full rounded-lg border-white dark:border-neutral-800 transition duration-200"
-                            src={`https://api.spoekle.com${clip.url}`}
-                            controls
-                          >
-                          </video>
-                        </div>
-                      </div>
-                      <div className="w-full flex justify-center bg-white dark:bg-neutral-900 transition duration-200 rounded-b-lg px-4 pt-2 pb-4">
-                        <button
-                          className="w-1/2 text-green-500 dark:text-green-800 flex items-center justify-center bg-neutral-100 dark:bg-neutral-800 hover:text-white hover:bg-green-500 dark:hover:bg-green-800 transition duration-300 py-2 px-6 rounded-l-md"
-                          onClick={() => upvoteClip(clip._id)}
-                        >
-                          <FaArrowUp className="mr-1" /> {clip.upvotes}
-                        </button>
-                        <button
-                          className="w-1/2 text-red-500 dark:text-red-800 flex items-center justify-center bg-neutral-100 dark:bg-neutral-800 hover:text-white hover:bg-red-500 dark:hover:bg-red-800 transition duration-300 py-2 px-6 rounded-r-md"
-                          onClick={() => downvoteClip(clip._id)}
-                        >
-                          <FaArrowDown className="mr-1" /> {clip.downvotes}
-                        </button>
-                      </div>
-                    </div>
-                  ))
-              ) : (
-                <div className="my-2 mx-4 text-center bg-black/30 p-4 rounded-md font-semibold text-xl text-white col-span-full">No clips available.</div>
-              )
-            )}
+                    ))
+                ) : (
+                  <div className="my-2 mx-4 text-center bg-black/30 p-4 rounded-md font-semibold text-xl text-white col-span-full">No clips available.</div>
+                )
+              )}
+            </div>
+
+            <RatedClips isLoggedIn={isLoggedIn} isLoading={isLoading} setExpandedClip={setExpandedClip} currentClips={currentClips} ratings={ratings} denyThreshold={denyThreshold} upvoteClip={upvoteClip} downvoteClip={downvoteClip} user={user} />
+
           </div>
-
-          <RatedClips isLoggedIn={isLoggedIn} isLoading={isLoading} setExpandedClip={setExpandedClip} currentClips={currentClips} ratings={ratings} denyThreshold={denyThreshold} upvoteClip={upvoteClip} downvoteClip={downvoteClip} user={user} />
-
         </div>
 
         <DeniedClips isLoggedIn={isLoggedIn} isLoading={isLoading} deniedClips={deniedClips} setExpandedClip={setExpandedClip} />
