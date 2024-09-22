6a375fb7580a12b174640685e492dc139799eb8c
Spoekle <thiangraber@gmail.com>
2024-09-22 19:04:59 +0200

    latest video begin

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import background from '../media/background.jpg';
 import banner1 from '../media/banner1.png';
+import { FaYoutube } from 'react-icons/fa';
+import cheerio from 'cheerio';
 
 function HomePage() {
+  const [latestVideo, setLatestVideo] = useState('');
+
+  useEffect(() => {
+    async function fetchLatestVideo() {
+      try {
+        const response = await fetch('https://www.youtube.com/playlist?list=PLwx5EB8PdMNf6dnNR7YCtOqWHkfU5kvFv');
+        const text = await response.text();
+        const $ = cheerio.load(text);
+        const videoId = $('a.yt-simple-endpoint.style-scope.ytd-playlist-video-renderer').first().attr('href').split('=')[1];
+        setLatestVideo(`https://www.youtube.com/watch?v=${videoId}`);
+      } catch (error) {
+        console.error('Error fetching latest video:', error);
+      }
+    }
+
+    fetchLatestVideo();
+  }, []);
+
   return (
     <div className="min-h-screen bg-neutral-200 dark:bg-neutral-900 text-neutral-900 dark:text-white relative">
       <div className="flex h-96 justify-center items-center" style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover' }}>
@@ -31,8 +51,33 @@ function HomePage() {
             <img src={banner1} alt="Banner" className='rounded-lg' />
           </div>
         </div>
-      </div>
-      <div className="flex-grow flex flex-col justify-center items-center" style={{ backgroundImage: `url(${banner1})`, backgroundSize: 'cover' }}>
+        <div className="container grid grid-cols-1 md:grid-cols-2 justify-center items-center w-full h-full">
+          <div className='w-auto m-4'>
+            {latestVideo ? (
+              <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${latestVideo}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Latest YouTube Video"
+              ></iframe>
+            ) : (
+              <p>Loading latest video...</p>
+            )}
+          </div>
+          <div className="flex flex-col justify-center items-center m-4 p-4 bg-neutral-300 dark:bg-neutral-950 rounded-lg">
+            <h1 className="text-3xl m-4 text-center">Watch the latest highlights here!</h1>
+            <div className="flex flex-col justify-between mt-8">
+              <Link to="https://www.youtube.com/@CubeCommunity">
+                <button className="bg-cc-red text-white font-bold py-3 px-6 rounded-lg shadow-lg transition duration-300">
+                  YouTube <FaYoutube className="inline" />
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
